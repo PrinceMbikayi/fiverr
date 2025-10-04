2447a25655461db92a8bb4eea83b0567398c91ff
Naimur Rahama <naimurrahaman248@gmail.com>
2025-10-05 02:17:19 +0600

    Add localization support for location permissions and implement rich notification service
    - Added German, English, Spanish, French, Italian, Dutch, Portuguese, and Dutch localization strings for location permission descriptions in InfoPlist.strings files.
    - Created link-assets-manifest.json to manage font assets.
    - Implemented rich notification service with NotificationService class, including plist and entitlements for the extension.
    - Added Firebase Messaging and Notifee integration for enhanced notification handling.

---
+++ ios/File.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  UmiiDEV
+//
+//  Created by Olivier PARIS on 08/10/2021.
+//  Copyright © 2021 Facebook. All rights reserved.
+//
+
+import Foundation
---
+++ ios/richNotification/NotificationService.h
@@ -0,0 +1,13 @@
+//
+//  NotificationService.h
+//  richNotification
+//
+//  Created by Olivier PARIS on 11/10/2021.
+//  Copyright © 2021 Facebook. All rights reserved.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
