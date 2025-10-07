d852602563b3894dcbbb4e69aca6a1c79b24cb30
Prince Mbikayi <pmbikayi01@gmail.com>
2025-10-07 14:22:13 +0300

    Issue fr RND

--- react-native.config.js
+++ react-native.config.js
@@ -1,8 +1,4 @@
 module.exports = {
-  dependencies: {
-    // Flipper configuration for Android
-    ...(process.env.NO_FLIPPER ? { 'react-native-flipper': { platforms: { android: { sourceDir: '../node_modules/react-native-flipper/android/src/main', packageImportPath: 'import io.invertase.flipper.ReactNativeFlipper;' } } } } : {}),
-  },
   assets: [
     './src/assets/fonts/',
   ],
